2c26e626061ff0f311d4b0a6f84914682ae9f050
prasojopxl <prasojodesigner@gmail.com>
2024-02-21 19:15:50 +0700

    first commit

---
+++ components/basecontent.jsx
@@ -0,0 +1,14 @@
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
+
+export default function Basecontent(props) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div>
+                Hello
+            </div>
+        </QueryClientProvider>
+    )
+}
