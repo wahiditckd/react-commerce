f310e191a13b0c2bbc7f865a07bfb93fe5bf0096
wahiditckd <wahid.itckd@gmail.com>
2024-07-03 21:03:11 +0700

    wa di footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import moment from "moment";
+import { FaWhatsapp } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Footer() {
   const currentYear = moment().format("YYYY");
   return (
-    <div className="bg-gray-800 text-white text-center py-3 text-sm">
-      &copy; {currentYear - 1} All Right Reserved. reactcommerce.com
-    </div>
+    <>
+      <div className="fixed bottom-3 right-3 text-4xl bg-slate-200 p-2 rounded-lg">
+        <Link href="https://wa.me/6281748219014">
+          <FaWhatsapp className="text-green-400" />
+        </Link>
+      </div>
+      <div className="bg-gray-800 text-white text-center py-3 text-sm">
+        &copy; {currentYear - 1} All Right Reserved. reactcommerce.com
+      </div>
+    </>
   );
 }
